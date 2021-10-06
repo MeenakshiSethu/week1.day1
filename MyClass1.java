0b2564a33df0a1fafa71684debd09ab194c905a8
MeenakshiSethu <91779320+MeenakshiSethu@users.noreply.github.com>
2021-10-06 07:24:40 +0530

    Add files via upload

---
+++ MyClass1.java
@@ -0,0 +1,17 @@
+
+public class MyClass1 {
+	
+	
+
+	public static void main(String[] args) {
+		
+		MyClass obj = new MyClass();
+		obj.add();
+		obj.subtraction();
+		//obj.multiplication();
+		//obj.division();
+		
+
+	}
+
+}
