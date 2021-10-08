028609753dc1708e24b09ab824cddc2bee34a042
MeenakshiSethu <91779320+MeenakshiSethu@users.noreply.github.com>
2021-10-08 08:18:09 +0530

    Add files via upload

---
+++ FindSecondLargest.java
@@ -0,0 +1,16 @@
+package week1day2;
+import java.util.Arrays;
+
+public class FindSecondLargest {
+
+
+	public static void main(String[] args) {
+		int[] arr = {3,2,11,4,6,7};
+		int length = arr.length;
+		Arrays.sort(arr);
+		System.out.println("The second largest number:" + arr[length - 2]);
+		
+
+	}
+
+}
